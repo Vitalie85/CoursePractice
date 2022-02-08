004c5937c22ead75300e9e9eb70730cabe3383b2
Vitalie ADOMNITA <v.adomnita@gmail.com>
2022-02-08 14:54:02 +0200

    Homework Lesson 7

---
+++ CoursePractice/src/homeWorkLesson7/DivisibleNumbers.java
@@ -0,0 +1,18 @@
+package homeWorkLesson7;
+
+public class DivisibleNumbers {
+    public static void main(String[] args) {
+        int j=0;
+        int i;
+        for (i = 100; i <= 1000; i++) {
+            if (i % 5 == 0 || i % 6 ==0) {
+                System.out.print(i + " ");
+                j++;
+                if (j == 10){
+                System.out.println( );
+                j = j-10;
+                }
+            }
+        }
+    }
+}
