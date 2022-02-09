447c83a1ebf8b4e7a180b432bd5f3d78f0416623
Vitalie ADOMNITA <v.adomnita@gmail.com>
2022-02-09 11:04:42 +0200

    Homework Lesson 7-updated

--- CoursePractice/src/homeWorkLesson7/DivisibleNumbers.java
+++ CoursePractice/src/homeWorkLesson7/DivisibleNumbers.java
@@ -4,9 +4,10 @@ public class DivisibleNumbers {
     public static void main(String[] args) {
         int j=0;
         int i;
+        System.out.print("Numbers divisible to 5 and 6 are: " +'\n');
         for (i = 100; i <= 1000; i++) {
-            if (i % 5 == 0 || i % 6 == 0) {
-                System.out.print(i + " ");
+            if (i % 5 == 0 && i % 6 == 0) {
+                System.out.print(i+ " " );
                 j++;
                 if (j == 10){
                 System.out.println( );
