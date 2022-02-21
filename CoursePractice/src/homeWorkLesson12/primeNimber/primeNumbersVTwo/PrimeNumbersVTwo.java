692bb51e1de5c3805d4da7f1a3e6b535d33a40e0
Vitalie ADOMNITA <v.adomnita@gmail.com>
2022-02-21 13:50:37 +0200

    HW lesson 12 updated

---
+++ CoursePractice/src/homeWorkLesson12/primeNimber/primeNumbersVTwo/PrimeNumbersVTwo.java
@@ -0,0 +1,40 @@
+package homeWorkLesson12.primeNimber.primeNumbersVTwo;
+
+
+public class PrimeNumbersVTwo {
+
+    public boolean primeNumbers(int number) {
+        if (!(number % 2 == 0) && number>1 || number / 2 == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PrimeNumbersVTwo prime = new PrimeNumbersVTwo();
+        int value = 10_000;
+        int count = 0;
+        int countPrints = 0;
+        int number = 0;
+        boolean numberIs;
+
+        for (int i = 0; i < value; i++) {
+            number = i;
+
+            numberIs = prime.primeNumbers(number);
+            if (numberIs) {
+                System.out.print(number + " ");
+                count++;
+                countPrints++;
+            }
+            if (countPrints == 30) {
+                System.out.println();
+                countPrints = 0;
+            }
+
+        }
+        System.out.println('\n' + "In range { 0; " + value + "} " + "were found " + count + " prime numbers.");
+    }
+}
+
+
